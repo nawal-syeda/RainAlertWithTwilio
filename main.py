69a8d7917206650c588663b73d4fb03f4e940afc
nawalfatima <45535355+nawalfatima@users.noreply.github.com>
2026-02-27 02:07:37 +0800

    api changed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-
+from dotenv import load_dotenv
 from twilio.rest import Client
 import os
+load_dotenv(dotenv_path=".env")
+
 OWM_API_KEY = os.environ.get("OWM_API_KEY")
 LAT = os.environ.get("LAT")
 LON = os.environ.get("LON")
@@ -12,20 +14,26 @@ TWILIO_TO_NUMBER = os.environ.get("TWILIO_TO_NUMBER")
 
 
 
+
 import requests
 parameters = {
-    "lat": LAT,
-    "lon": LON,
-    "appid": OWM_API_KEY,
-    "cnt":4
+    "latitude": LAT,
+    "longitude": LON,
+    "hourly": "weathercode",
+    "forecast_days": 1
 }
-response = requests.get(url = "https://api.openweathermap.org/data/2.5/forecast", params=parameters)
-response.raise_for_status()
+response = requests.get(
+    "https://api.open-meteo.com/v1/forecast",params=parameters)
+
+print(response.json())
+
 weather_data = response.json()
+
 going_to_rain = False
-for item in weather_data["list"]:
-    if item["weather"][0]["id"]<700:
+for code in weather_data["hourly"]["weathercode"][:4]:
+    if code >= 51:  # 51+ means rain/drizzle/snow
         going_to_rain = True
+
 if going_to_rain:
     print("bring an umbrella")
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
@@ -34,8 +42,11 @@ if going_to_rain:
         body="It's going to rain in the next few hours, don't forget to bring an umbrella.",
         from_=TWILIO_FROM_NUMBER,
         to=TWILIO_TO_NUMBER,
+
     )
 
     print(message.body)
     print(message.status)
+
+
 
